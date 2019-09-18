d8f1f68a4707500762bcd48472c03e392456f8f9
maldan <blackwanted@yandex.ru>
2019-09-18 17:16:58 +0300

    -- Update

--- test/test.c
+++ test/test.c
@@ -1,3 +1,12 @@
+#include "../osoyan.h"
+
 int main() {
+    MEMORY_INIT;
+
+    NEW_STRING(X);
+    string_add(X, "hello");
+    string_add(X, "world");
+    assert(strcmp(X->list, "helloworld") == 0);
+
     return 0;
 }
